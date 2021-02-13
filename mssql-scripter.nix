726ece312570e1a0632a3ac3f9cc53ed1da768ef
Robert K. Bell <robert.k.bell@gmail.com>
2021-02-14 10:00:31 +1100

    chore: include the service layer (broken, fhs?)

--- mssql-scripter.nix
+++ mssql-scripter.nix
@@ -14,6 +14,9 @@ p3p.buildPythonApplication {
     sha256 = "0xb4yhqizsdbfrg5hh96phb4ghxrzln3kkkblbkvqah05jrk2cm6";
   };
 
+  # About patching the MS service layer binaries, See Also:
+  # <https://github.com/NixOS/nixpkgs/blob/63bffaf22799bef43d23112f9065a207bf3aef24/pkgs/applications/misc/azuredatastudio/default.nix#L52>
+
   propagatedBuildInputs = with p3p; [ future ];
 
   checkPhase = "exit 0";
@@ -23,6 +26,13 @@ p3p.buildPythonApplication {
     export HOME=./temp-home
     mkdir -p "$out"/{bin,src}
     cp -R * $out/src
+
+    # don't need to keep the archive files in the closure
+    rm -rf $out/src/sqltoolsservice
+
+    mkdir -p $out/src/mssqlscripter/mssqltoolsservice/bin
+    tar -xvf sqltoolsservice/manylinux1/Microsoft.SqlTools.ServiceLayer-linux-x64-netcoreapp2.1.tar.gz \
+        -C $out/src/mssqlscripter/mssqltoolsservice/bin
 
     makeWrapper ${python3Packages.python.interpreter} $out/bin/mssql-scripter \
       --set PYTHONPATH "$PYTHONPATH:$out/src" \
